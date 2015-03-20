f12f1dbe4ff0b42be2c8e0a77c49488f23723904
huajianwei2013@outlook.com <huajianwei2013@outlook.com>
2015-03-20 14:25:27 +0800

    change the folder

--- test.py
+++ test.py
@@ -1,17 +1,30 @@
 __author__ = 'huajw'
 #coding=utf-8
-#只是为了验证导入模块而已
 
-def ():
-    try:
-        if 1 == 1:
-            print "you have import the module"
-        else:
-            print "error"
-    except:
-        print "the module is error"
+import os
+import sys
+import time
 
 
+from com.android.monkeyrunner import MonkeyRunner,MonkeyDevice, MonkeyImage
+from com.android.monkeyrunner.easy import By
+from com.android.chimpchat.hierarchyviewer import HierarchyViewer
+print "import the module end"
+
+
+# start the connect
+try:
+    device=MonkeyRunner.waitForConnection(1.0 , 'emulator-5554')
+    #device=MonkeyRunner.waitForConnection(1.0 , 'emulator-5554')
+    print  'The device has already connect'
+except:
+    print "The devices can't connect ,please reboot the phone"
+
+
+print "ok"
+
+MonkeyRunner.sleep(3)
+devices.MonkeyRunner.alert("ok","stop")
 
 
 
